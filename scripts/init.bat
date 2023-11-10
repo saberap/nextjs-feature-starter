e4a76d0a70b2e903d4faf577d09824404d0468e3
Saber Alipour <alipouor@gmail.com>
2023-11-10 15:56:49 +0330

    Initial Commit

---
+++ scripts/init.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set /p createDirectory=Do you want to install dependencies? (y/n): 
+
+if /i "%createDirectory%"=="y" (
+    npm i @reduxjs/toolkit antd dayjs formik yup jalaliday numeral react-redux redux-persist async-mutex prettier husky lint-staged
+    echo The packages were installed successfully :)
+) else (
+    echo The packages were not installed :(
+)
+
+set /p copyFiles=Do you want to copy files to the new directory? (y/n): 
+
+if /i "%copyFiles%"=="y" (
+    wsl tar -xf ./cli.tar 
+    cp -r ./cli/common ../src/
+    echo common created successfully.
+    cp -r ./cli/modules ../src/
+    echo modules created successfully.
+    cp -r ./cli/redux ../src/
+    echo redux created successfully.
+    cp -r ./cli/styles ../src/
+    echo styles created successfully.
+    cp -f ./cli/files/.prettierrc.js ../
+    cp -f ./cli/files/next.config.js ../
+    cp -f ./cli/files/layout.tsx ../src/app
+    cp -rf ./cli/files/.husky ../
+    cp -rf ./cli/files/.vscode ../
+    cp -rf ./cli/files/.lintstagedrc.js ../
+    cp -rf ./cli/files/.env ../
+    cp -rf ./cli/scripts/* .
+    rm -rf ./cli
+    echo Files copied successfully.
+) else (
+    echo File copying aborted.
+)
+
+pause
+
+exit
